f058ca56455bc363f2378598794b5b3b3274cd4e
rene <reneguardia@outlook.com>
2022-07-26 19:45:31 -0300

    Refactor

--- src/main/java/alkemy/disney/service/impl/EmailServiceImpl.java
+++ src/main/java/alkemy/disney/service/impl/EmailServiceImpl.java
@@ -8,7 +8,9 @@ import com.sendgrid.SendGrid;
 import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +22,11 @@ public class EmailServiceImpl implements EmailService {
 
     private final Environment environment;
 
-    private String emailSender = null;
+    @Value("${alkemy.icons.email.sender}")
+    private String emailSender;
 
-    private boolean enable = Boolean.FALSE;
+    @Value("${alkemy.icons.email.enabled}")
+    private boolean enable;
 
     @Autowired
     public EmailServiceImpl(Environment environment) {
@@ -31,24 +35,28 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendWelcomeTo(String to) {
-        if (!enable) {
-            return;
+        if (enable) {
+            String apiKey = environment.getProperty("EMAIL_API_KEY");
+            Email fromEmail = new Email(emailSender);
+            Email toEmail = new Email(to);
+            Content content = new Content("text/plain", "Welcome to Alkemy Disney");
+            String subject = "Alkemy Disney";
+            Mail mail = new Mail(fromEmail, subject, toEmail, content);
+            sendEmail(mail, apiKey);
         }
-        String apiKey = environment.getProperty("EMAIL_API_KEY");
-        Email fromEmail = new Email(emailSender);
-        Email toEmail = new Email(to);
-        Content content = new Content("text/plain", "Welcome to Alkemy Disney");
-        String subject = "Alkemy Disney";
-        Mail mail = new Mail(fromEmail, subject, toEmail, content);
-        SendGrid sendGrid = new SendGrid(apiKey);
-        Request request = new Request();
+    }
+
+    private void sendEmail(@NotNull Mail mail,
+                           @NotNull String apiKey) {
         try {
+            SendGrid sendGrid = new SendGrid(apiKey);
+            Request request = new Request();
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
             Response response = sendGrid.api(request);
         } catch (IOException ex) {
-            System.out.println("Error trying to send the Email");
+            ex.getMessage();
         }
     }
 
